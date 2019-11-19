af493720a3cd363fda693e14c2a09ccb8a41ce9f
Hi-guo <1448284254@qq.com>
2019-11-19 16:45:53 +0800

    1

---
+++ 333.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main(void)
+ {
+     int n,i;
+     double term=1,sum=0;
+     printf("Enter n:\n");
+     scanf("%d",&n);
+     i=1;
+     while(i<=n)
+     {
+        term=term*i;
+         sum+=1/term;
+         i++;
+     }
+     sum+=1;
+     printf("sum =%.5lf",sum);
+     return 0;
+ }
